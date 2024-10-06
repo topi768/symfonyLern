e20df088e37899bb24fc9ba1fa626052e41a930e
topi768 <topi768@inbox.ru>
2024-10-06 13:07:43 +1000

    add 1 task

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,9 +6,40 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController  extends AbstractController
 {
+    #[Route('/prime/{number}', methods: ["GET"])]
+    public function checkPrimeNumber($number): JsonResponse
+    {
+        $isPrime = $this->isPrime($number);
+
+        return new JsonResponse([
+            'number' => $number,
+            'is_prime' => $isPrime,
+            'message' => $isPrime ? 'Это простое число' : 'Это составное число'
+        ]);
+    }
+    private function isPrime(int $number): bool
+    {
+        if ($number <= 1) {
+            return false;
+        }
+        if ($number == 2) {
+            return true;
+        }
+        if ($number % 2 == 0) {
+            return false;
+        }
+        for ($i = 3; $i <= sqrt($number); $i += 2) {
+            if ($number % $i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     #[Route('/hello/{name}', methods: ['GET'])]
     public function index(string $name): Response
     {
